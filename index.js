688adbaeea432ff8568a2efa9bc84149950a1df6
Bradley J. Spaulding <brad.spaulding@gmail.com>
2020-04-05 20:08:25 -0700

    Revert "Extracted function for main checking logic"
    This reverts commit ac22f3dd22ee3a66d93e2f596efdce1adad8c8e1.

--- index.js
+++ index.js
@@ -38,13 +38,11 @@ const delay = (ms) =>
   });
 
 const login = async (page) => {
-	console.log("Heading to instacart.com...");
   await page.goto("https://instacart.com");
   const loginButtonSelector = "header button";
   await page.waitFor(loginButtonSelector);
   await page.click(loginButtonSelector);
 
-	console.log(`Logging in as ${process.env.INSTACART_EMAIL}...`);
   const emailFieldSelector = "input[type=email]";
   await page.waitFor(emailFieldSelector);
   await page.type(emailFieldSelector, process.env.INSTACART_EMAIL);
@@ -72,29 +70,6 @@ const checkForDeliveries = async (page) => {
 
 const say = (str) => require("child_process").execSync(`say ${str}`);
 
-const checkUntilFoundOrFailed = async page => {
-	let foundDelivery = false;
-	let numExceptions = 0;
-	while (!foundDelivery && numExceptions < 3) {
-		console.log("Checking for deliveries...");
-		try {
-			foundDelivery = await checkForDeliveries(page);
-			if (!foundDelivery) {
-				console.log("none found, waiting...");
-				await delay(60 * 1000);
-			}
-			numExceptions = 0;
-		} catch (e) {
-			console.error("Checking failed this time, waiting...", e);
-			numExceptions += 1;
-			if (numExceptions < 3) {
-				await delay(60 * 1000);
-			}
-		}
-	}
-	return numExceptions < 3 ? Promise.resolve() : Promise.reject();
-};
-
 (async () => {
   const browser = await puppeteer.launch({ headless: !(process.env.SLOWCART_HEADLESS === "false"), args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   try {
@@ -102,14 +77,34 @@ const checkUntilFoundOrFailed = async page => {
 
     await login(page);
 
-    try {
-			await checkUntilFoundOrFailed(page);
-			console.log("Found a delivery time!");
-			sendSMS("Found a delivery time!");
-    } catch (e) {
+    let foundDelivery = false;
+    let numExceptions = 0;
+    while (!foundDelivery && numExceptions < 3) {
+      console.log("Checking for deliveries...");
+      try {
+        foundDelivery = await checkForDeliveries(page);
+        if (!foundDelivery) {
+          console.log("none found, waiting...");
+          await delay(60 * 1000);
+        }
+				numExceptions = 0;
+      } catch (e) {
+        console.error("Checking failed this time, waiting...", e);
+        numExceptions += 1;
+        if (numExceptions < 3) {
+          await delay(60 * 1000);
+        }
+      }
+    }
+    if (numExceptions < 3) {
+      while (true) {
+        sendSMS("Found a delivery time!");
+        await delay(5 * 1000);
+      }
+    } else {
       await browser.close();
-      console.error("Failed to check too many times, exiting");
-      sendSMS("Failed to check too many times, exiting");
+      console.error("Failed with exceptions 3 times, exiting");
+      sendSMS("Failed with exceptions 3 times, exiting");
     }
   } catch (e) {
     console.error(e);
